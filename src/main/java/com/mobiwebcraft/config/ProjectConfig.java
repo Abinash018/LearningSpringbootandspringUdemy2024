a8e552713be494dd6951b559c5b344a1b13c8936
Lenovo <gabinash018@gmail.com>
2024-04-09 18:34:58 +0530

    3rd commit with IOC container and contest commnet

--- src/main/java/com/mobiwebcraft/config/ProjectConfig.java
+++ src/main/java/com/mobiwebcraft/config/ProjectConfig.java
@@ -4,8 +4,23 @@ import com.mobiwebcraft.beans.Vehicle;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+
+
+
+// this annotation configuration will be automaticcally call by the ioc container at the beans cration tinme
+
+
+
+
+
+
 @Configuration
 public class ProjectConfig {
+
+    // due to the configuration what happen  it will look at the bean annoted tag , and it will look at the type of the annotation class
+
+    // it will got in the parameter so what happen accodingly as per the requirement basis it will call the proper fuinction accoding to the\
+    // type of the class mentioned
 
     @Bean
     Vehicle  vehicle(){
