20c33092e27e840d20f6a0bc221ba91790abf9b8
Lenovo <gabinash018@gmail.com>
2024-04-10 17:22:03 +0530

    xml bean

--- src/main/java/com/mobiwebcraft/main/Example1.java
+++ src/main/java/com/mobiwebcraft/main/Example1.java
@@ -4,6 +4,7 @@ import com.mobiwebcraft.beans.Vehicle;
 import com.mobiwebcraft.config.ProjectConfig;
 import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.util.Random;
 import java.util.function.Supplier;
@@ -16,56 +17,59 @@ public class Example1 {
 //        vehicle.setName("Honda City");
 //        System.out.println("Vehicle Name from non spring context is : " + vehicle.getName());
 
-        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+     //   var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+        var context = new ClassPathXmlApplicationContext("beans.xml");
+         Vehicle vehicle =  context.getBean(Vehicle.class);
+         System.out.println("vehicle name from spring Context is "+vehicle.getName());
 
-
-        Vehicle volkswagen = new Vehicle();
-        volkswagen.setName("Volkswagen");
-        Supplier<Vehicle> volkswagenSupplier = () -> volkswagen;
-
-        Supplier<Vehicle> audiSupplier = () -> {
-
-             Vehicle audi = new Vehicle();
-             audi.setName("Audi");
-             return audi;
-        };
-
-
-        Random random = new Random();
-        int randomNumber = random.nextInt(10);
-        System.out.println("Random number: " + randomNumber);
-
-
-        if((randomNumber % 2) == 0) {
-
-            context.registerBean("volkswagen", Vehicle.class, volkswagenSupplier);
-        }
-
-        else {
-            context.registerBean("audi", Vehicle.class,audiSupplier);
-
-        }
-
-        Vehicle volksVehicle = null ;
-        Vehicle audiVehicle = null ;
-
-        try {
-
-            volksVehicle = context.getBean("volkswagen", Vehicle.class);
-
-        }
-        catch (NoSuchBeanDefinitionException noSuchBeanDefinitionException){
-
-            System.out.println("Error while creating Volkswagen vehicle ") ;
-        }
-
-        try{
-            audiVehicle = context.getBean("audi", Vehicle.class);
-
-        }
-        catch (NoSuchBeanDefinitionException noSuchBeanDefinitionException){
-            System.out.println("Error while creating Audi vehicle ") ;
-        }
+        //
+//        Vehicle volkswagen = new Vehicle();
+//        volkswagen.setName("Volkswagen");
+//        Supplier<Vehicle> volkswagenSupplier = () -> volkswagen;
+//
+//        Supplier<Vehicle> audiSupplier = () -> {
+//
+//             Vehicle audi = new Vehicle();
+//             audi.setName("Audi");
+//             return audi;
+//        };
+//
+//
+//        Random random = new Random();
+//        int randomNumber = random.nextInt(10);
+//        System.out.println("Random number: " + randomNumber);
+//
+//
+//        if((randomNumber % 2) == 0) {
+//
+//            context.registerBean("volkswagen", Vehicle.class, volkswagenSupplier);
+//        }
+//
+//        else {
+//            context.registerBean("audi", Vehicle.class,audiSupplier);
+//
+//        }
+//
+//        Vehicle volksVehicle = null ;
+//        Vehicle audiVehicle = null ;
+//
+//        try {
+//
+//            volksVehicle = context.getBean("volkswagen", Vehicle.class);
+//
+//        }
+//        catch (NoSuchBeanDefinitionException noSuchBeanDefinitionException){
+//
+//            System.out.println("Error while creating Volkswagen vehicle ") ;
+//        }
+//
+//        try{
+//            audiVehicle = context.getBean("audi", Vehicle.class);
+//
+//        }
+//        catch (NoSuchBeanDefinitionException noSuchBeanDefinitionException){
+//            System.out.println("Error while creating Audi vehicle ") ;
+//        }
 
 //        //Vehicle veh = context.getBean("audivehicle",Vehicle.class);
 //        Vehicle veh = context.getBean(Vehicle.class);
